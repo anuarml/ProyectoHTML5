e810148ffa5d124a2a51ad56f1012ac0485db115
anuarml <anuar_ml93@hotmail.com>
2015-06-19 12:03:08 -0500

    Proyecto Base

---
+++ js/radius.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){ 	
+						   
+						   
+	// radius Box
+	$('.menu_nav ul li a').css({"border-radius": "6px", "-moz-border-radius":"6px", "-webkit-border-radius":"6px"});
+	$('ul.fmenu li a').css({"border-radius": "6px", "-moz-border-radius":"6px", "-webkit-border-radius":"6px"});
+	// end radius Box
+	 
+});	
+
+$('#register').on("click",function(){
+	window.location = "registro.html";
+});
